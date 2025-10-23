556c3ec6a231e9ee0dc78735ff75e0d1db50b8d5
Hao <hao.streamnz@gmail.com>
2025-10-23 16:37:23 +1300

    init project

---
+++ src/main/java/com/streamnz/qapractise1/QaPractise1Application.java
@@ -0,0 +1,13 @@
+package com.streamnz.qapractise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaPractise1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaPractise1Application.class, args);
+    }
+
+}
